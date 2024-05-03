c6962d2461c428a6b8a5210c1b965b7c882a721c
xXehub <artixsanni@gmail.com>
2024-05-03 17:40:49 +0700

    first commit

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -37,7 +37,11 @@
                     </div>
                 </div>
                 <hr>
-
+                <div class="row">
+                    <div class="col-md-12 d-grid">
+                        <a href="{{ route('home') }}" class="btn btn-outline-dark btn-lg mt-3"><i class="bi-arrow-left-circle me-2"></i> Back</a>
+                    </div>
+                </div>
             </div>
         </div>
 
